aeac5a4e1ecdff443115ab625a0e44ea2f76d893
victoryus84 <victorcostiuc@gmail.com>
2026-03-29 21:59:18 +0300

    Add PostalAddress and Addresses fields to Client model

--- backend/internal/models/models.go
+++ backend/internal/models/models.go
@@ -48,14 +48,16 @@ type ClientType struct {
 type Client struct {
 	gorm.Model
 	UUIDModel    `gorm:"embedded"`
-	ClientTypeID uint       `gorm:"not null"`                          // Foreign key to ClientType
-	ClientType   ClientType `gorm:"foreignKey:ClientTypeID;not null"`  // Tipul clientului ("individual", "company", etc.)
-	Name         string     `gorm:"type:varchar(100);not null"`        // Numele clientului
-	FiscalID     string     `gorm:"type:varchar(15);unique;not null"`  // Codul fiscal al clientului (unic)
-	Email        string     `gorm:"type:varchar(100);unique;not null"` // Email-ul clientului (unic)
-	Phone        string     `gorm:"type:varchar(50)"`                  // Telefonul clientului
-	Address      string     `gorm:"type:text"`                         // Adresa clientului
-	Contracts    []Contract `gorm:"foreignKey:ClientID"`               // Contractele clientului
+	ClientTypeID  uint       `gorm:"not null"`                          // Foreign key to ClientType
+	ClientType    ClientType `gorm:"foreignKey:ClientTypeID;not null"`  // Tipul clientului ("individual", "company", etc.)
+	Name          string     `gorm:"type:varchar(100);not null"`        // Numele clientului
+	FiscalID      string     `gorm:"type:varchar(15);unique;not null"`  // Codul fiscal al clientului (unic)
+	Email         string     `gorm:"type:varchar(100);unique;not null"` // Email-ul clientului (unic)
+	Phone         string     `gorm:"type:varchar(50)"`                  // Telefonul clientului
+	Address 	  string     `gorm:"type:text"`                         // Adresa clientului
+	PostalAddress string     `gorm:"type:text"`                   		// Adresa postala a clientului
+	Contracts     []Contract `gorm:"foreignKey:ClientID"`               // Contractele clientului
+	Addresses     []ClientAddress `gorm:"foreignKey:ClientID"`          // Adresele asociate clientului
 }
 
 // ****************************************************
@@ -66,8 +68,8 @@ type ClientAddress struct {
 	UUIDModel  `gorm:"embedded"`
 	Address    string   `gorm:"type:text;not null"`                  // Adresa
 	Type       string   `gorm:"type:varchar(50)"`                    // Tipul adresei ("billing", "shipping" etc.)
-	ClientID uint     `gorm:"not null"`                            // Cheie externă către Client
-	Client   Client `gorm:"foreignKey:ClientID;references:ID"` // Clientul
+	ClientID uint     `gorm:"not null"`                              // Cheie externă către Client
+	Client   Client `gorm:"foreignKey:ClientID;references:ID"`       // Clientul
 	OwnerID    uint     `gorm:"not null"`                            // ID-ul ownerului (utilizatorului)
 	Owner      User     `gorm:"foreignKey:OwnerID;references:ID"`    // Ownerul adresei
 }
